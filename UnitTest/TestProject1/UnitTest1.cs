3e19206849562c48f2c6238efeb88b1714ab4671
Jorge Nápoles <jnapoles@gmail.com>
2021-06-10 00:50:49 -0500

    Update

--- UnitTest/TestProject1/UnitTest1.cs
+++ UnitTest/TestProject1/UnitTest1.cs
@@ -1,19 +1,57 @@
+using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 
 namespace TestProject1
 {
+    public interface ILog
+    {
+        void Write(string mensaje);
+    }
+    public class ConsoleLog:ILog         
+    {
+        public void Write(string mensaje)
+        {
+            Console.WriteLine(mensaje);
+        }
+    }
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+        private readonly ILog log;
+        public BankAccount(ILog _log)
+        {
+            this.log = _log;
+        }
+
+        public void Deposit(int deposit)
+        {
+            Balance += deposit;
+        }
+    }
+
     [TestFixture]
     public class Tests
     {
         [SetUp]
         public void Setup()
         {
+            //Test1(4, 6);
         }
 
         [Test]
-        public void Test1()
+        public void Test1( )
         {
-            Assert.Pass();
+            //var resultado = a + b;
+            //Assert.That(4+6,Is.EqualTo(7));  // Chequeo dado condiciones
+            //Assert.Fail("Error en las pruebas"); // Lanzar el Fail
+            //Assert.Inconclusive("Hola falta info"); // Lanzar dado prueba inconclusa una alerta en naranja
+            //Assert.Warn("Prueba inconclusa"); // Lanza un mensaje de prueba similar al inconcluso
+
+
+            
+
+            Warn.If(2 + 2 != 5);
         }
     }
 }
